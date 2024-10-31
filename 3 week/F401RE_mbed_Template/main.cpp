6578984ae3bd11bddbe1ed196e23687151b7b79f
Mystique2023 <ryanshin0336@gmail.com>
2024-10-31 16:58:20 +0900

    week8 lab2

--- 3 week/F401RE_mbed_Template/main.cpp
+++ 3 week/F401RE_mbed_Template/main.cpp
@@ -1,6 +1,7 @@
 #include "mbed.h"
 
 AnalogIn analog_value(A0);
+DigitalOut myled(LED1);
 #define operating_voltage 5
 
 float measured_voltage;
@@ -10,7 +11,14 @@ int main(){
 	while(1){
 		measured_voltage = analog_value.read();
 		output_voltage = measured_voltage*operating_voltage;
-		printf("output voltage = %.3f \r\n", output_voltage);
+		
+		if (output_voltage > 2) {
+			myled = 1;
+			printf("output voltage = %.3f \r\n", output_voltage);
+		} else {
+			myled = 0;
+			printf("output voltage = %.3f \r\n", output_voltage);
+		}
 		wait(0.5);
 	}
 }
