4e0259ead99085d5c9daccbc538fa4cea90fb1e6
Mystique2023 <ryanshin0336@gmail.com>
2024-11-14 16:57:08 +0900

    assign PB_7 to button1 and button2

--- Week 11/Week 11 - Interrupts (assignment)/main.cpp
+++ Week 11/Week 11 - Interrupts (assignment)/main.cpp
@@ -1,7 +1,7 @@
 #include "mbed.h"
 #include "motordriver.h"
 
-InterruptIn button1(PA_7);
+InterruptIn button1(PB_7);
 InterruptIn button2(PB_7);
 InterruptIn button3(PC_4);
 Serial pc(USBTX, USBRX);
@@ -25,35 +25,38 @@ void change_direction() {
 
 void acc() {
 	if(speed < 1) {
-
+		speed += 0.1; 
 		pc.printf("Increase Speed to %3.1f\r\n", speed);
 	} else {
-		pc.printf("Reached MAX Speed (No Increase)\r\n");
+		pc.printf("Reached Max Speed (No Increase)\r\n");
 	}
 	print_current_state();
-
+	wait(0.2); 
 }
 
-void dec() {
-	if(speed > 0.2) {
 
+void dec() {
+	if (speed>0.2) {
+		speed -= 0.1;
 		pc.printf("Decrease Speed to %3.1f\r\n", speed);
 	} else {
-		pc.printf("Reached MIN Speed (No Decrease)\r\n");
+		pc.printf("Reached MIN speed (No Decrease)\r\n");
 	}
 	print_current_state();
-
+	wait(0.2);
 }
 
 int main() {
 	print_current_state();
-
+    button1.fall(&change_direction);  
+    button2.fall(&acc);
+	button3.fall(&dec);
 	A.forward(speed);
 	while(1) {
 		if(direction == 1) {
 			A.forward(speed);
-		} else{
-
+		} else {
+			A.backward(speed);
 		}
 	}
 }
